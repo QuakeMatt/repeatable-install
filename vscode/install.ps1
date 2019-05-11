83453a2c571995f3139b75dbf293f55cd18b0615
Matthew Lawrence-Stubley <quakematt@outlook.com>
2019-05-11 20:23:36 +0100

    Add VSCode associations

--- vscode/install.ps1
+++ vscode/install.ps1
@@ -1,7 +1,18 @@
 Write-Output "Installing VSCode..."
 
+$app = 'vscode'
+
 # Install VSCode
-scoop install vscode
+scoop install $app
 
 # Install settings-sync extension
 code.cmd --install-extension 'Shan.code-settings-sync'
+
+# Associate with files
+$iconPath = (Resolve-Path "$PSScriptRoot\..\icons").Path
+$codePath = "$env:USERPROFILE\scoop\apps\$app\current\Code.exe"
+$regContent = (Get-Content "$PSScriptRoot\associations.reg" -Raw).
+    Replace("%ICONPATH%", $iconPath.Replace("\", "\\")).
+    Replace("%CODEPATH%", $codePath.Replace("\", "\\"))
+Set-Content -Path "$env:TEMP\associations.reg" -Value $regContent
+reg import "$env:TEMP\associations.reg"
