7859763a9ec1a3da74a2c83b65ff07d28583306b
Matthew Lawrence-Stubley <quakematt@outlook.com>
2019-09-12 19:31:31 +0100

    Refactor, add more explorer settings

--- explorer/install.ps1
+++ explorer/install.ps1
@@ -1,8 +1,10 @@
 Write-Output "Configuring explorer..."
 
 reg import "$PSScriptRoot\add-user-folder.reg"
-reg import "$PSScriptRoot\explorer-settings.reg"
-reg import "$PSScriptRoot\sound-settings.reg"
+reg import "$PSScriptRoot\apply-explorer-settings.reg"
+reg import "$PSScriptRoot\apply-sound-settings.reg"
+reg import "$PSScriptRoot\remove-3d-objects.reg"
+reg import "$PSScriptRoot\remove-duplicate-drives.reg"
 
 Write-Output "Restarting explorer..."
 Stop-Process -ProcessName explorer
