78de9fc28ac35181a996e4b596892b77875abdfb
Matthew Lawrence-Stubley <quakematt@outlook.com>
2020-01-16 21:16:44 +0000

    Fix explorer tweaks not being applied

---
+++ explorer/install-admin.ps1
@@ -0,0 +1,2 @@
+reg import "$PSScriptRoot\remove-3d-objects.reg"
+reg import "$PSScriptRoot\remove-duplicate-drives.reg"
--- explorer/install.ps1
+++ explorer/install.ps1
@@ -3,8 +3,10 @@ Write-Output "Configuring explorer..."
 reg import "$PSScriptRoot\add-user-folder.reg"
 reg import "$PSScriptRoot\apply-explorer-settings.reg"
 reg import "$PSScriptRoot\apply-sound-settings.reg"
-reg import "$PSScriptRoot\remove-3d-objects.reg"
-reg import "$PSScriptRoot\remove-duplicate-drives.reg"
+
+if (Test-Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}') {
+    sudo "$PSScriptRoot\install-admin.ps1"
+}
 
 Write-Output "Restarting explorer..."
 Stop-Process -ProcessName explorer
